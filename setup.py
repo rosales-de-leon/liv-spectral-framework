10eba5cfb9919359fc417aae7b8f4296fb692b6d
rosales-de-leon <albertoros.lodz@gmail.com>
2026-06-17 18:18:09 +0200

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="liv_spectral_framework",
+    version="0.1.0",
+    author="Alberto Rosales de Leon",
+    description="Framework for spectral LIV studies using EBL absorption in blazar spectra",
+    packages=find_packages(),
+    python_requires=">=3.9.21",
+)
